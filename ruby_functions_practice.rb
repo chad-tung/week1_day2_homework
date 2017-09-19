ec10114bbbbc32628f19b76a3d96824cb176666b
chad-tung <chad.tung@gmail.com>
2017-09-19 14:27:57 +0100

    debugged up to join_string

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -9,3 +9,19 @@ end
 def subtract(num_1, num_2)
   return num_1 - num_2
 end
+
+def multiply(num_1, num_2)
+  return num_1 * num_2
+end
+
+def divide(num_1, num_2)
+  return num_1 / num_2
+end
+
+def length_of_string(sentence)
+  return sentence.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
